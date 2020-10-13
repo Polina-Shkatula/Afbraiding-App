0da8473d3949f904d4a91b8ab91009ee66e780af
PolinaShkatula <polina.shkatula@mailbox.tu-dresden.de>
2020-10-13 13:47:03 +0200

    created two activities: SplashActivity and SignInActivity renew Manifest connected two activities: SplashActivity opens, waits 2 secs and switch on SignInActivity

---
+++ app/src/main/java/com/example/afbraidingapp/ui/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.afbraidingapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import java.util.*;
+
+import com.example.afbraidingapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
