b9d14ff573e0a6e4cfe88ac543f0c9c0081657c3
PolinaShkatula <polina.shkatula@mailbox.tu-dresden.de>
2020-10-25 09:11:36 +0100

    added logic to AddProfilePictureActivity created two resources bg_btn_60_disabled2.xml and selector_btn_60_2.xml used glide in AddProfilePictureActivity

--- app/src/main/java/com/example/afbraidingapp/Congratulation.java
+++
@@ -1,4 +0,0 @@
-package com.example.afbraidingapp;
-
-public class Congratulation {
-}
--- app/src/main/java/com/example/afbraidingapp/ui/AddProfilePictureActivity.java
+++ app/src/main/java/com/example/afbraidingapp/ui/AddProfilePictureActivity.java
@@ -1,16 +1,69 @@
 package com.example.afbraidingapp.ui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.afbraidingapp.R;
 
 public class AddProfilePictureActivity extends AppCompatActivity {
+    public static final int PICK_IMAGE = 9;
+    Button button;
+    ImageView imageView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_profile_picture);
+
+        button = (Button)findViewById(R.id.btnSave);
+        imageView = (ImageView)findViewById(R.id.btnAddPhoto);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(imageView !=  null){
+                    openNewActivity();
+                }
+            }
+        });
+    }
+
+    public void openNewActivity(){
+        Intent intent = new Intent(this, CongratulationActivity.class);
+        startActivity(intent);
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE && data != null){
+            Uri imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+            Glide.with(this).load(imageUri).circleCrop().into(imageView);
+            button.setEnabled(true);
+        }
     }
 }
