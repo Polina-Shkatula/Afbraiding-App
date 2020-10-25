667dc199250f1ac0b557c1b8341afbbc5a7b35af
PolinaShkatula <polina.shkatula@mailbox.tu-dresden.de>
2020-10-25 09:54:11 +0100

    separated numbers in number phone

--- app/src/main/java/com/example/afbraidingapp/ui/SignInActivity.java
+++ app/src/main/java/com/example/afbraidingapp/ui/SignInActivity.java
@@ -36,17 +36,51 @@ public class SignInActivity extends AppCompatActivity {
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charSequence == null || charSequence.length() == 0) {
+                    return;
+                }
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int j = 0; j<charSequence.length(); j++) {
+                    if (j != 3 && j != 8 && charSequence.charAt(j) == ' ') {
+                        continue;
+                    } else {
+                        stringBuilder.append(charSequence.charAt(j));
+                        if ((stringBuilder.length() == 4 || stringBuilder.length() == 9)
+                                && stringBuilder.charAt(stringBuilder.length() - 1) != ' ') {
+                            stringBuilder.insert(stringBuilder.length() - 1, ' ');
+                        }
+                    }
+                }
+                if (!stringBuilder.toString().equals(charSequence.toString())) {
+                    int index = i + 1;
+                    if (stringBuilder.charAt(i) == ' ') {
+                        if (i1 == 0) {
+                            index++;
+                        } else {
+                            index--;
+                        }
+                    } else {
+                        if (i1 == 1) {
+                            index--;
+                        }
+                    }
+                    editText.setText(stringBuilder.toString());
+                    editText.setSelection(index);
+
+                }
+            }
 
             @Override
             public void afterTextChanged(Editable editable) {
                 int length = editable.length();
-                button.setEnabled(length == 12);
-                if(length > 12) editText.setError("Invalid number");
+                button.setEnabled(length == 13);
+                if(length > 13) editText.setError("Invalid number");
             }
         });
 
     }
+
     public void openNewActivity(){
         Intent intent = new Intent(this, CodeVerificationActivity.class);
         startActivity(intent);
